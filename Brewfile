10c5dd70ac4f10afce72acd0af57502b5e631981
Grant Butler <git@grantjbutler.com>
2017-02-11 10:28:04 -0500

    Move brew installations to Brewfile.

---
+++ Brewfile
@@ -0,0 +1,39 @@
+tap 'caskroom/cask'
+
+# Tools
+brew 'carthage'
+brew 'diff-so-fancy'
+brew 'fish'
+brew 'git-lfs'
+brew 'mas'
+brew 'wget'
+
+# Apps
+cask '1password'
+cask 'alfred'
+cask 'bartender'
+cask 'coderunner'
+cask 'couleurs'
+cask 'dash'
+cask 'dropbox'
+cask 'fluid'
+cask 'github-desktop'
+cask 'hex-fiend'
+cask 'istat-menus'
+cask 'iterm2'
+cask 'paw'
+cask 'sketch'
+cask 'slack'
+cask 'spotify'
+cask 'the-hit-list'
+cask 'visual-studio-code'
+cask 'zeplin'
+
+# App Store
+mas 'Fantastical 2', id: 975937182
+mas 'Keynote', id: 409183694
+mas 'Numbers', id: 409203825
+mas 'Pages', id: 409201541
+mas 'Tweetbot', id: 557168941
+mas 'xScope', id: 889428659
+
--- install.sh
+++ install.sh
@@ -7,59 +7,14 @@ echo "Installing Homebrew..."
 curl -fsSL -o /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/master/install
 /usr/bin/ruby /tmp/brew-install
 brew doctor
+brew update
 
-# Install fish
-echo "Installing Fish..."
-brew install fish
+brew tap homebrew/bundle
+brew bundle
 
 echo "Setting Fish as Defualt Shell..."
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
 
-# Install Diff So Fancy
-echo "Installing Diff So Fancy..."
-brew install diff-so-fancy
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
-# Install git-lfs
-echo "Installing git-lfs..."
-brew install git-lfs
-
-# Install carthage
-echo "Installing carthage..."
-brew install carthage
-
-# Install wget
-echo "Installing wget..."
-brew install wget
-
-# Install cask
-echo "Installing cask..."
-brew tap caskroom/cask
-
-# Install mas
-echo "Installing mas..."
-brew install mas
-
-# Install apps
-apps=(1password alfred bartender coderunner couleurs dash dropbox fluid github-desktop hex-fiend istat-menus iterm2 paw sketch slack spotify the-hit-list visual-studio-code zeplin)
-
-echo "The following apps will be installed:"
-for app in ${apps[@]}; do
-  echo "- ${app}"
-done
-
-echo "Verify the above apps before installing by visiting https://github.com/caskroom/homebrew-cask/blob/master/Casks/<app>.rb to verify."
-read -p "Press [Enter] to install the above apps."
-
-for app in ${apps[@]}; do
-  brew cask install $app
-done
-
-# App Store Apps
-#                  Fantastical 2    Keynote      Numbers     Pages      Tweetbot    xScope
-app_store_app_ids=("975937182"      "409183694" "409203825" "409201541" "557168941" "889428659")
-echo "Installing apps from the app store"
-for app_id in ${app_store_app_ids[@]}; do
-  mas install ${app_id}
-done
