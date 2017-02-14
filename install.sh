0287e0a776bbf6e1ba8778a27c7ca2ce75e181b3
Grant Butler <grantjbutler@users.noreply.github.com>
2017-02-14 18:49:11 -0500

    Extract setting up brew to its own file.

--- install.sh
+++ install.sh
@@ -2,16 +2,7 @@
 echo "Installing Xcode Command Line Tools..."
 xcode-select --install
 
-# Install homebrew
-echo "Installing Homebrew..."
-curl -fsSL -o /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/master/install
-/usr/bin/ruby /tmp/brew-install
-brew doctor
-brew update
-
-brew tap homebrew/bundle
-brew bundle
-
+./setup/brew
 ./setup/fish
 ./setup/git
 ./setup/macos
