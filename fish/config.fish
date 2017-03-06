8eeff6ab3ec4b37d203ce5217b785146c5577197
Grant Butler <grantjbutler@users.noreply.github.com>
2017-03-05 20:51:28 -0500

    Fix config.fish syntax error.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 set -x PATH (brew --prefix homebrew/php/php71)/bin $HOME/.composer/vendor/bin $PATH
-set -X EDITOR vim
+set -x EDITOR vim
 
 status --is-interactive; and source (rbenv init -|psub)
 
