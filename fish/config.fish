4be5da7a19ac5292405be729e9ce7846abe7ba71
Grant Butler <grantjbutler@users.noreply.github.com>
2017-03-07 18:52:58 -0500

    Add dotfiles fish function path.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 set -x PATH (brew --prefix homebrew/php/php71)/bin $HOME/.composer/vendor/bin $PATH
 set -x EDITOR vim
+set -x fish_function_path $fish_function_path ~/.dotfiles/fish/functions
 
 status --is-interactive; and source (rbenv init -|psub)
 
