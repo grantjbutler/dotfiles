6294ae41c480de2ca5e1d7ee1f7ab19eee46d60b
Grant Butler <grantjbutler@users.noreply.github.com>
2018-06-26 19:24:36 -0400

    Install Xcode using xcode-install.

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash -eu
 
-echo "Installing Xcode Command Line Tools..."
-xcode-select --install
+echo "Installing Xcode..."
+
+install_dir=`mktemp -d`
+pushd $install_dir
+curl -sL -O https://github.com/neonichu/ruby-domain_name/releases/download/v0.5.99999999/domain_name-0.5.99999999.gem
+gem install domain_name-0.5.99999999.gem
+gem install --conservative xcode-install
+rm -f domain_name-0.5.99999999.gem
+popd
+
+xcversion install 9.4.1
+xcversion install-cli-tools
 
 ./setup/brew
 ./setup/fish
