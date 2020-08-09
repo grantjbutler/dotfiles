56afb478681748bb305c486185f8f9eccfd6dad9
Grant Butler <grantjbutler@users.noreply.github.com>
2020-08-09 11:22:48 -0400

    Give composer more memory.

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 set -x PATH ~/.dotfiles/bin (brew --prefix php)/bin $HOME/.composer/vendor/bin $PATH
 set -x EDITOR vim
 set -x fish_function_path ~/.dotfiles/fish/functions $fish_function_path
+set -x COMPOSER_MEMORY_LIMIT -1
 
 alias git=hub
 alias cat=bat
