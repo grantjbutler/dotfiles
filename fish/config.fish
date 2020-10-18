9ca6a1a304049cc18521833b27bd2da8c1a88231
Grant Butler <grantjbutler@users.noreply.github.com>
2020-10-18 11:35:19 -0400

    Tweak path for installing composer with homebrew.

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -x PATH ~/.dotfiles/bin (brew --prefix php)/bin $HOME/.composer/vendor/bin $PATH
+set -x PATH ~/.dotfiles/bin $PATH
 set -x EDITOR vim
 set -x fish_function_path ~/.dotfiles/fish/functions $fish_function_path
 set -x COMPOSER_MEMORY_LIMIT -1
