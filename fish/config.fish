10ddd9e385e7e28d70ae8a86c5c71ce913c2b005
Grant Butler <grantjbutler@users.noreply.github.com>
2021-06-12 12:14:51 -0400

    Update how the PATH is set.

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
-set -x PATH ~/.dotfiles/bin $PATH
+fish_add_path ~/.dotfiles/bin
+fish_add_path /usr/local/bin
+fish_add_path /usr/local/sbin
+
 set -x EDITOR vim
 set -x fish_function_path ~/.dotfiles/fish/functions $fish_function_path
 set -x COMPOSER_MEMORY_LIMIT -1
