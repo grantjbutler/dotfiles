fb92003acbddb9b3a0fc8b44194d2da7fee16102
Grant Butler <grantjbutler@users.noreply.github.com>
2021-06-23 13:57:04 -0400

    Replace hub with gh.

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,6 @@ set -x EDITOR vim
 set -x fish_function_path ~/.dotfiles/fish/functions $fish_function_path
 set -x COMPOSER_MEMORY_LIMIT -1
 
-alias git=hub
 alias cat=bat
 
 status --is-interactive; and source (rbenv init -|psub)
