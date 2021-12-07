4fdfe83ead412e144c1ab4c9555dc8d00084242c
Grant J. Butler <grantjbutler@users.noreply.github.com>
2021-12-06 22:59:47 -0500

    Change origin URL after setup.

--- install.sh
+++ install.sh
@@ -18,3 +18,6 @@ xcversion install 13.1
 ./setup/vscode
 ./setup/xcode
 ./setup/macos
+
+# Since, for the first time, we're likely to have cloned over HTTPS, update the URL for the repo to the SSH URL.
+git remote set-url origin git@github.com:grantjbutler/dotfiles.git
