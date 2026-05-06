6094782d6447bd50e54f928741e03de32fac5e2e
Grant J. Butler <grantjbutler@users.noreply.github.com>
2026-05-06 12:22:38 -0400

    Replace asdf with mise.

--- fish/config.fish
+++ fish/config.fish
@@ -3,20 +3,12 @@ if test -e /opt/homebrew/bin/brew
 end
 
 fish_add_path "$HOME/.dotfiles/bin"
-fish_add_path /usr/local/bin
-fish_add_path /usr/local/sbin
 
 set -x EDITOR vim
 set -x fish_function_path ~/.dotfiles/fish/functions $fish_function_path
 set -x COMPOSER_MEMORY_LIMIT -1
 
 alias cat="bat --style=numbers"
-
-if test -e /opt/homebrew/opt/asdf/libexec/asdf.fish
-  source /opt/homebrew/opt/asdf/libexec/asdf.fish
-else if test -e /usr/local/opt/asdf/libexec/asdf.fish
-  source /usr/local/opt/asdf/libexec/asdf.fish
-end
 
 fish_add_path (yarn global bin)
 fish_add_path "$HOME/.composer/vendor/bin"
